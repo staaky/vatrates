9ef65c5ff8f68bc6024b1bf66b3150d780d84892
Nick Stakenburg <nick@nickstakenburg.com>
2014-12-21 19:19:44 +0100

    1.0.0

---
+++ README.md
@@ -0,0 +1,23 @@
+# VAT Rates
+
+_Up-to-date European VAT Rates_
+
+As of 1 January 2015 new EU VAT regulations change the way in which VAT (consumer tax) is charged on digital services. Instead of the tax rate being applied according to where the supplier is located, the seller must determine where the buyer is located and apply the relevant local rate of tax to the purchaser.
+
+See: http://ec.europa.eu/taxation_customs/taxation/vat/how_vat_works/telecom
+
+This repository provides files in different formats (Javascript, JSON & PHP) to help you apply these local VAT rates.
+
+## Updates
+
+VAT rates are updated using data provided by the [European Commission](http://ec.europa.eu/taxation_customs/resources/documents/taxation/vat/how_vat_works/rates/vat_rates_en.pdf) and [VATLive.com](http://www.vatlive.com).
+
+If you notice anything out of date or would like to contribute, please create an issue or a pull request.
+
+## License
+
+Everything is [MIT Licensed](http://choosealicense.com/licenses/mit/), have at it!
+
+* * *
+
+[Nick Stakenburg](http://www.nickstakenburg.com)
---
+++ vatrates.js
@@ -0,0 +1,302 @@
+/*!
+ * VATRates - v1.0.0
+ * MIT License
+ */
+
+(function(window) {
+
+var VATRates = {
+  "AT": {
+    "country": "Austria",
+    "rates": {
+      "superReduced": false,
+      "reduced": [10],
+      "standard": 20,
+      "parking": 12
+    }
+  },
+
+  "BE": {
+    "country": "Belgium",
+    "rates": {
+      "superReduced": false,
+      "reduced": [6, 12],
+      "standard": 21,
+      "parking": false
+    }
+  },
+
+  "BG": {
+    "country": "Bulgaria",
+    "rates": {
+      "superReduced": false,
+      "reduced": [9],
+      "standard": 20,
+      "parking": false
+    }
+  },
+
+  "CY": {
+    "country": "Cyprus",
+    "rates": {
+      "superReduced": false,
+      "reduced": [5, 9],
+      "standard": 19,
+      "parking": false
+    }
+  },
+
+  "CZ": {
+    "country": "Czech Republic",
+    "rates": {
+      "superReduced": false,
+      "reduced": [15],
+      "standard": 21,
+      "parking": false
+    }
+  },
+
+  "DE": {
+    "country": "Germany",
+    "rates": {
+      "superReduced": false,
+      "reduced": [7],
+      "standard": 21,
+      "parking": false
+    }
+  },
+
+  "DK": {
+    "country": "Denkmark",
+    "rates": {
+      "superReduced": false,
+      "reduced": false,
+      "standard": 25,
+      "parking": false
+    }
+  },
+
+  "EE": {
+    "country": "Estonia",
+    "rates": {
+      "superReduced": false,
+      "reduced": [9],
+      "standard": 20,
+      "parking": false
+    }
+  },
+
+  "EL": {
+    "country": "Greece",
+    "rates": {
+      "superReduced": false,
+      "reduced": [6.5, 13],
+      "standard": 23,
+      "parking": false
+    }
+  },
+
+  "ES": {
+    "country": "Spain",
+    "rates": {
+      "superReduced": 4,
+      "reduced": [10],
+      "standard": 21,
+      "parking": false
+    }
+  },
+
+  "FI": {
+    "country": "Finland",
+    "rates": {
+      "superReduced": false,
+      "reduced": [10, 14],
+      "standard": 24,
+      "parking": false
+    }
+  },
+
+  "FR": {
+    "country": "France",
+    "rates": {
+      "superReduced": 2.1,
+      "reduced": [5.5, 10],
+      "standard": 20,
+      "parking": false
+    }
+  },
+
+  "HR": {
+    "country": "Croatia",
+    "rates": {
+      "superReduced": false,
+      "reduced": [5,13],
+      "standard": 25,
+      "parking": false
+    }
+  },
+
+  "HU": {
+    "country": "Hungary",
+    "rates": {
+      "superReduced": false,
+      "reduced": [5, 18],
+      "standard": 27,
+      "parking": false
+    }
+  },
+
+  "IE": {
+    "country": "Ireland",
+    "rates": {
+      "superReduced": 4.8,
+      "reduced": [9, 13.5],
+      "standard": 23,
+      "parking": 13.5
+    }
+  },
+
+  "IT": {
+    "country": "Italy",
+    "rates": {
+      "superReduced": 4,
+      "reduced": [10],
+      "standard": 22,
+      "parking": false
+    }
+  },
+
+  "LT": {
+    "country": "Lithuania",
+    "rates": {
+      "superReduced": false,
+      "reduced": [5, 9],
+      "standard": 21,
+      "parking": false
+    }
+  },
+
+  "LU": {
+    "country": "Luxembourg",
+    "rates": {
+      "superReduced": 3,
+      "reduced": [6, 12],
+      "standard": 17,
+      "parking": 12
+    }
+  },
+
+  "LV": {
+    "country": "Latvia",
+    "rates": {
+      "superReduced": false,
+      "reduced": [12],
+      "standard": 21,
+      "parking": false
+    }
+  },
+
+  "MT": {
+    "country": "Malta",
+    "rates": {
+      "superReduced": false,
+      "reduced": [5, 7],
+      "standard": 18,
+      "parking": false
+    }
+  },
+
+  "NL": {
+    "country": "Netherlands",
+    "rates": {
+      "superReduced": false,
+      "reduced": [6],
+      "standard": 21,
+      "parking": false
+    }
+  },
+
+  "PL": {
+    "country": "Poland",
+    "rates": {
+      "superReduced": false,
+      "reduced": [5, 8],
+      "standard": 23,
+      "parking": false
+    }
+  },
+
+  "PT": {
+    "country": "Portugal",
+    "rates": {
+      "superReduced": false,
+      "reduced": [6, 13],
+      "standard": 23,
+      "parking": 13
+    }
+  },
+
+  "RO": {
+    "country": "Romania",
+    "rates": {
+      "superReduced": false,
+      "reduced": [5, 9],
+      "standard": 24,
+      "parking": false
+    }
+  },
+
+  "SE": {
+    "country": "Sweden",
+    "rates": {
+      "superReduced": false,
+      "reduced": [6, 12],
+      "standard": 25,
+      "parking": false
+    }
+  },
+
+  "SI": {
+    "country": "Slovenia",
+    "rates": {
+      "superReduced": false,
+      "reduced": [9.5],
+      "standard": 22,
+      "parking": false
+    }
+  },
+
+  "SK": {
+    "country": "Slovakia",
+    "rates": {
+      "superReduced": false,
+      "reduced": [10],
+      "standard": 20,
+      "parking": false
+    }
+  },
+
+  "UK": {
+    "country": "United Kingdom",
+    "rates": {
+      "superReduced": false,
+      "reduced": [5],
+      "standard": 20,
+      "parking": false
+    }
+  }
+};
+
+// export
+if (typeof define === 'function' && define.amd) {
+  // AMD
+  define(VATRates);
+} else if (typeof exports === 'object') {
+  // CommonJS
+  module.exports = VATRates;
+} else {
+  // global
+  window.VATRates = VATRates;
+}
+
+})(this);
